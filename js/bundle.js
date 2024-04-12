56c54f08414ca9cc6c7e4f8c85a0492e9a7ee7f1
Huacal92 <huacaldesign@gmail.com>
2024-04-12 08:32:41 -0500

    Ajustes generales del sitio web, se crea la validacion del formulario en javascript

--- js/bundle.js
+++ js/bundle.js
@@ -1,5 +1,30 @@
 'use strict';
 
+/*
+ * Translated default messages for the jQuery validation plugin.
+ * Locale: ES
+ */
+ 
+jQuery.extend(jQuery.validator.messages, {
+    required: "Este campo es obligatorio.",
+    remote: "Por favor, rellena este campo.",
+    email: "Por favor, escribe una dirección de correo válida",
+    url: "Por favor, escribe una URL válida.",
+    date: "Por favor, escribe una fecha válida.",
+    dateISO: "Por favor, escribe una fecha (ISO) válida.",
+    number: "Por favor, escribe un número entero válido.",
+    digits: "Por favor, escribe sólo dígitos.",
+    creditcard: "Por favor, escribe un número de tarjeta válido.",
+    equalTo: "Por favor, escribe el mismo valor de nuevo.",
+    accept: "Por favor, escribe un valor con una extensión aceptada.",
+    maxlength: jQuery.validator.format("Por favor, no escribas más de {0} caracteres."),
+    minlength: jQuery.validator.format("Por favor, no escribas menos de {0} caracteres."),
+    rangelength: jQuery.validator.format("Por favor, escribe un valor entre {0} y {1} caracteres."),
+    range: jQuery.validator.format("Por favor, escribe un valor entre {0} y {1}."),
+    max: jQuery.validator.format("Por favor, escribe un valor menor o igual a {0}."),
+    min: jQuery.validator.format("Por favor, escribe un valor mayor o igual a {0}.")
+  });
+
 /* Menu Toggle  */
 //Main navigation Active Class Add Remove
 $(".navbar-toggler").on("click", function () {
@@ -274,4 +299,68 @@ $(document).ready(function() {
 			}
 		}
 	});
+});
+
+$.validator.addMethod( "emailRegex", function (value, element, regexp) {
+
+    let emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+    return this.optional(element) || emailRegex.test(value);
+    
+}, "Por favor, introduce un correo electrónico válido.");
+
+$.validator.addMethod( "textRegex", function (value, element, textexp) {
+
+    let textRegex = /^[a-zA-Z0-9\s\-.,áéíóúÁÉÍÓÚüÜñÑ]*$/;
+    return this.optional(element) || textRegex.test(value);
+    
+}, "Por favor, solo se permiten letras, números y algunos caracteres especiales como espacios, guiones, puntos y comas.");
+
+
+
+$('#formularioContacto').validate({
+    rules:{
+        nombre:{
+            required: true,
+            minlength:3,
+            maxlength: 30,
+            textRegex: true
+        },
+        email:{
+            required: true,
+            email: true,
+            emailRegex: true
+        },
+        asunto:{
+            required: true,
+            minlength: 5,
+            maxlength: 100,
+            textRegex: true
+        },
+        mensaje:{
+            required: true,
+            minlength: 5,
+            maxlength: 200,
+            textRegex: true
+        }
+    }
+});
+
+
+
+
+$('#buttonSubmit').click(function (){
+    if ($('#formularioContacto').valid() == false){
+        return;
+    }else {
+        $('#mensajeExito').css('display', 'block');
+        $('#buttonSubmit').attr('disabled', true);
+        /* $('#formularioContacto').trigger('reset'); */
+        $('#formularioContacto')[0].reset();
+        setTimeout(function() {
+            $('#mensajeExito').css('display', 'none');
+          }, 5000);
+    }
+    $('email').val();
+    $('asunto').val();
+    $('mensaje').val();
 });
